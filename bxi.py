d93901bdfb89af2c66ff25d7ab0a1176d6dc1ce3
parvezkaziws <74939229+parvezkaziws@users.noreply.github.com>
2020-11-24 06:30:29 +0600

    Delete bxi.py

--- bxi.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system("clear")
-print 3*("\n")
-print(" This tool is not available right now\n\n we are working on it\n\n to get update please subscribe our\n\n youtube channel TRICKPROOF\n\n open the following link\n\n https://www.youtube.com/channel/UC5kptDqLxcVCzHgayaBuBPA")
